d247f4cc564bc6c2d48aae9ab0a105e2dd3c497e
Rohnak Agarwal <rrka79wal@gmail.com>
2021-05-23 22:39:35 +0530

    plot 1

---
+++ README.md
@@ -0,0 +1,7 @@
+# Matplotlib
+
+
+## References
+
+ - [Official Website](https://matplotlib.org/)
+ - [Format Strings of plot](https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.plot.html)
---
+++ f01_demo.py
@@ -0,0 +1,31 @@
+from matplotlib import pyplot as plt
+from f01_demo_data import *
+
+
+if __name__ == '__main__':
+    # plt.style.use('fivethirtyeight')
+    # plt.style.use('ggplot')
+    plt.xkcd()
+
+    # plt.plot(ages_x, dev_y, 'k--', label='All Devs')
+    # plt.plot(ages_x, py_dev_y, 'b', label='Python Devs')
+    # plt.plot(ages_x, dev_y, color='k', linestyle='--', marker='.' ,label='All Devs')
+    # plt.plot(ages_x, py_dev_y, color='b', marker='o' ,label='Python Devs')
+    # plt.plot(ages_x, py_dev_y, color='#5a7d9a', linewidth=3, label='Python Devs')
+    # plt.plot(ages_x, js_dev_y, color='#adad3b', linewidth=3, label='JS Devs')
+    plt.plot(ages_x, py_dev_y, label='Python Devs')
+    plt.plot(ages_x, js_dev_y, label='JS Devs')
+    plt.plot(ages_x, dev_y, color='#444444', linestyle='--', label='All Devs')
+
+    plt.xlabel('Ages')
+    plt.ylabel('Median Salary (USD)')
+    plt.title('Median Salary (USD) by age')
+
+    # plt.legend(['All Devs', 'Python Devs'])
+    plt.legend()
+    # plt.grid(True)
+    # plt.tight_layout()
+
+    plt.savefig('plot02.png')
+
+    plt.show()
