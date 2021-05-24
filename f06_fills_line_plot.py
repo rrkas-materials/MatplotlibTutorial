5a0ecfd50b085e5c734a4c1e8f6ee5c25c97dfbe
Rohnak Agarwal <rrka79wal@gmail.com>
2021-05-24 09:52:00 +0530

    fill line

---
+++ f06_fills_line_plot.py
@@ -0,0 +1,67 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+
+if __name__ == '__main__':
+    data = pd.read_csv('data/data06.csv')
+    ages = data['Age']
+    dev_salaries = data['All_Devs']
+    py_salaries = data['Python']
+    js_salaries = data['JavaScript']
+
+    plt.plot(ages, dev_salaries, color='#444444',
+             linestyle='--', label='All Devs')
+
+    plt.plot(ages, py_salaries, label='Python')
+
+    overall_median = 57287
+
+    # plt.fill_between(
+    #     ages,
+    #     py_salaries,
+    #     overall_median,
+    #     alpha=0.25,
+    #     where=(py_salaries > overall_median),
+    #     interpolate=True,
+    #     color='green'
+    # )
+    # plt.fill_between(
+    #     ages,
+    #     py_salaries,
+    #     overall_median,
+    #     alpha=0.25,
+    #     where=(py_salaries <= overall_median),
+    #     interpolate=True,
+    #     color='red',
+    # )
+    plt.fill_between(
+        ages,
+        py_salaries,
+        dev_salaries,
+        alpha=0.25,
+        where=(py_salaries > dev_salaries),
+        interpolate=True,
+        color='green',
+        label='Above AVG',
+    )
+    plt.fill_between(
+        ages,
+        py_salaries,
+        dev_salaries,
+        alpha=0.25,
+        where=(py_salaries <= dev_salaries),
+        interpolate=True,
+        color='red',
+        label='Below AVG'
+    )
+
+
+
+    plt.legend()
+
+    plt.title('Median Salary (USD) by Age')
+    plt.xlabel('Ages')
+    plt.ylabel('Median Salary (USD)')
+
+    plt.tight_layout()
+
+    plt.show()
